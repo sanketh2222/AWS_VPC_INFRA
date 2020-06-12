27afa8ad59373c33e6bb72c3302b623890e2b3b7
sanketh2222 <sankeths94@gmail.com>
2020-06-12 22:36:49 +0530

    vpc commit

--- app.py
+++ app.py
@@ -2,10 +2,22 @@
 
 from aws_cdk import core
 
-from new.new_stack import NewStack
+#from new.new_stack import NewStack
+
+from resource_stack.customvpc_stack import Customvpc
+
+
 
 
 app = core.App()
-NewStack(app, "new")
+
+#env_US=core.Environment(account=app.node.try_get_context('prod')['account'],region=app.node.try_get_context('prod')['region'])
+#env_Europe=core.Environment(account=app.node.try_get_context('dev')['account'],region=app.node.try_get_context('dev')['region'])
+
+#print(app.node.try_get_context('dev')['region'])
+#print(app.node.try_get_context('@aws-cdk/core:enableStackNameDuplicates'))
+Customvpc(app, "myvpcstack")
+#NewStack(app, "mydevstack1",env=env_US)
+#NewStack(app,"myprodstack1",is_prod=True,env=env_US)
 
 app.synth()
--- new/new_stack.py
+++ new/new_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import core
-
+from aws_cdk import aws_s3
+from aws_cdk import aws_ec2
+from aws_cdk import aws_kms as _kms
 
 class NewStack(core.Stack):
 
@@ -7,3 +9,39 @@ class NewStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        
+        print(self.node.try_get_context('prod')['kms_arn'])
+        
+        kms_key=_kms.Key.from_key_arn(
+            self,
+            "mykeyid",
+            self.node.try_get_context('prod')['kms_arn']
+        )
+        
+        
+        
+        vpc_id=self.node.try_get_context('envs')['prod']
+        mask=vpc_id['vpc_configs']['mask']
+        
+        print(mask)
+        print(kms_key)
+        print()
+        
+        if is_prod:
+            artifact=aws_s3.Bucket(
+                self,
+                "Myprodbuck",
+                 block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
+                 encryption=aws_s3.BucketEncryption.KMS,
+                 encryption_key=kms_key,
+                 removal_policy=core.RemovalPolicy.RETAIN
+            )
+        else:
+            aws_s3.Bucket
+            (
+                 self,
+                "pdid"
+             )
+
+            
+
